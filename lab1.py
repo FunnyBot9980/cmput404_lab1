c94246cccc352787ace554b789bc6a679bf430e7
funnybot9980 <macjohnny56@gmail.com>
2023-01-11 19:51:08 -0700

    implemented the script downloading itself

--- lab1.py
+++ lab1.py
@@ -3,3 +3,7 @@ import requests
 print(requests.__version__)
 response = requests.get("https://google.com")
 print(response)
+
+response = requests.get("https://raw.githubusercontent.com/FunnyBot9980/cmput404_labs/main/lab1.py")
+
+open("file.txt", "w").write(response.content)
