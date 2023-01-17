bbfc271c6cb54beff0970020190115d0079dc6ab
John Macdonald <macjohnny56@gmail.com>
2023-01-17 15:38:52 -0700

    changed url to match with new repo name

--- lab1.py
+++ lab1.py
@@ -4,6 +4,6 @@ print(requests.__version__)
 response = requests.get("https://google.com")
 print(response)
 
-response = requests.get("https://raw.githubusercontent.com/FunnyBot9980/cmput404_labs/main/lab1.py")
+response = requests.get("https://raw.githubusercontent.com/FunnyBot9980/cmput404_lab1/main/lab1.py")
 
 open("lab1.py", "wb").write(response.content)
